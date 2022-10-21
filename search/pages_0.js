ed7ae6a46f7d02efec780e29821366febe5bfd7e
arutar <arutar@users.noreply.github.com>
2022-10-21 14:28:27 +0000

    deploy: b96427c841fd3fbd67cf27d4c235b314b2b23fa5

--- search/pages_0.js
+++ search/pages_0.js
@@ -1,5 +1,4 @@
 var searchData=
 [
-  ['wininfparser_0',['wininfparser',['../index.html',1,'']]],
-  ['wininfparser_20v1_2e0_1',['wininfparser v1.0',['../md__r_e_a_d_m_e.html',1,'']]]
+  ['wininfparser_0',['wininfparser',['../index.html',1,'(Global Namespace)'],['../md__r_e_a_d_m_e.html',1,'(Global Namespace)']]]
 ];
