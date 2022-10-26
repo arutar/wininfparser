09e00ed3416b65edf793c1ca24c8d3fbc68aa35f
arutar <arutar@users.noreply.github.com>
2022-10-26 13:01:23 +0000

    deploy: 5a300035c08abb9b8c82c3bfbbfd4538ab1140ac

--- search/all_b.js
+++ search/all_b.js
@@ -5,11 +5,12 @@ var searchData=
   ['searchvalueiter_2',['SearchValueIter',['../classwininfparser_1_1_i_n_fsection.html#a34c64cfa5a0d807850e983e3799ad106',1,'wininfparser::INFsection']]],
   ['sections_3',['Sections',['../classwininfparser_1_1_win_i_n_f.html#a38601fccc62ba63fb72272fc76056d3d',1,'wininfparser::WinINF']]],
   ['setindent_4',['SetIndent',['../classwininfparser_1_1_i_n_fsection.html#a2a97eac7c30e6fa09cbee8d58b9c3d65',1,'wininfparser::INFsection']]],
-  ['setname_5',['SetName',['../classwininfparser_1_1_i_n_fsection.html#a8d649c964963af2cc5320a66ecc79eb2',1,'wininfparser::INFsection']]],
-  ['setnamecomment_6',['SetNameComment',['../classwininfparser_1_1_i_n_fsection.html#a5b392b14f917a99ee413478efe231bde',1,'wininfparser::INFsection']]],
-  ['setnext_7',['SetNext',['../classwininfparser_1_1_i_n_fsection.html#a0e8225f2fdd3adfa7c557744151d2856',1,'wininfparser::INFsection']]],
-  ['setprevious_8',['SetPrevious',['../classwininfparser_1_1_i_n_fsection.html#ac3940ccaf64818970d4386c458d5ae15',1,'wininfparser::INFsection']]],
-  ['settype_9',['SetType',['../classwininfparser_1_1_i_n_fsection.html#a16148a0e2838dd8005760723d8154b84',1,'wininfparser::INFsection']]],
-  ['setvalid_10',['SetValid',['../classwininfparser_1_1_i_n_fsection.html#a243ae8e0f22b53ded0b28683335ea3ae',1,'wininfparser::INFsection']]],
-  ['single_5fline_11',['single_line',['../classwininfparser_1_1_i_n_fsection.html#a147fe06c2014a29bbf5b6eea9df59153',1,'wininfparser::INFsection']]]
+  ['setkeyautosize_5',['SetKeyAutoSize',['../classwininfparser_1_1_i_n_fsection.html#acc902afccc331b74b5697d6217949392',1,'wininfparser::INFsection']]],
+  ['setname_6',['SetName',['../classwininfparser_1_1_i_n_fsection.html#a8d649c964963af2cc5320a66ecc79eb2',1,'wininfparser::INFsection']]],
+  ['setnamecomment_7',['SetNameComment',['../classwininfparser_1_1_i_n_fsection.html#a5b392b14f917a99ee413478efe231bde',1,'wininfparser::INFsection']]],
+  ['setnext_8',['SetNext',['../classwininfparser_1_1_i_n_fsection.html#a0e8225f2fdd3adfa7c557744151d2856',1,'wininfparser::INFsection']]],
+  ['setprevious_9',['SetPrevious',['../classwininfparser_1_1_i_n_fsection.html#ac3940ccaf64818970d4386c458d5ae15',1,'wininfparser::INFsection']]],
+  ['settype_10',['SetType',['../classwininfparser_1_1_i_n_fsection.html#a16148a0e2838dd8005760723d8154b84',1,'wininfparser::INFsection']]],
+  ['setvalid_11',['SetValid',['../classwininfparser_1_1_i_n_fsection.html#a243ae8e0f22b53ded0b28683335ea3ae',1,'wininfparser::INFsection']]],
+  ['single_5fline_12',['single_line',['../classwininfparser_1_1_i_n_fsection.html#a147fe06c2014a29bbf5b6eea9df59153',1,'wininfparser::INFsection']]]
 ];
