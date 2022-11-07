3b6aef1f98277060d85ef5e2546ac26630fe4707
arutar <arutar@users.noreply.github.com>
2022-11-07 14:05:21 +0000

    deploy: a72e8c62f86873fc1ab8da056d1ea1503626ee48

--- search/functions_1.js
+++ search/functions_1.js
@@ -1,8 +1,8 @@
 var searchData=
 [
-  ['addcomment_0',['AddComment',['../classwininfparser_1_1_i_n_fsection.html#ab0ad95cf8cd1d289aca39026b526c438',1,'wininfparser::INFsection']]],
-  ['adddata_1',['AddData',['../classwininfparser_1_1_i_n_fsection.html#a35829464d83165317b071e0c69cd3dda',1,'wininfparser::INFsection']]],
-  ['adddatap_2',['AddDataP',['../classwininfparser_1_1_i_n_fsection.html#ab344703fdd2df46e1da4a7bf466e8fc5',1,'wininfparser::INFsection']]],
+  ['addcomment_0',['AddComment',['../classwininfparser_1_1_i_n_fsection.html#ab7cf6791e15acaff713d7554e11158c1',1,'wininfparser::INFsection']]],
+  ['adddata_1',['AddData',['../classwininfparser_1_1_i_n_fsection.html#a13b671280b3928d89b2821af12bdc5a3',1,'wininfparser::INFsection']]],
+  ['adddatap_2',['AddDataP',['../classwininfparser_1_1_i_n_fsection.html#a8005328cfd87cf3d8194089ce01972b8',1,'wininfparser::INFsection']]],
   ['addemptystrings_3',['AddEmptyStrings',['../classwininfparser_1_1_i_n_fsection.html#a124c00149ee36e3497169d938889d7ef',1,'wininfparser::INFsection']]],
   ['addsection_4',['AddSection',['../classwininfparser_1_1_win_i_n_f.html#aee7a763c05c72c7134be75d5d37a94bf',1,'wininfparser::WinINF']]]
 ];
