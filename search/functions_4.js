f5d6fa1c80e55fd4349d999e86be971ee635856d
arutar <arutar@users.noreply.github.com>
2022-11-08 15:59:15 +0000

    deploy: fcea1667e2460962de9da01c482012d80d1a7f3b

--- search/functions_4.js
+++ search/functions_4.js
@@ -1,10 +1,11 @@
 var searchData=
 [
-  ['getfilename_0',['GetFileName',['../classwininfparser_1_1_win_i_n_f.html#ac3209adfcee8d0861bc2b334692a1b0b',1,'wininfparser::WinINF']]],
-  ['getindent_1',['GetIndent',['../classwininfparser_1_1_i_n_fsection.html#a4431d920ce5b7834643d6fdcf2d3dcc7',1,'wininfparser::INFsection']]],
-  ['getkeyindex_2',['GetKeyIndex',['../classwininfparser_1_1_i_n_fsection.html#a60d50fe4fa59cda676ae3cacdbe6570c',1,'wininfparser::INFsection']]],
-  ['getname_3',['GetName',['../classwininfparser_1_1_i_n_fsection.html#a7bbf9ec71e0a6bdf4e9bd6b69e298d84',1,'wininfparser::INFsection']]],
-  ['getsection_4',['GetSection',['../classwininfparser_1_1_win_i_n_f.html#a935a049bb0a9a993833ed5c6ab7d8cde',1,'wininfparser::WinINF']]],
-  ['gettype_5',['GetType',['../classwininfparser_1_1_i_n_fsection.html#a965250b8ca2a217bb3531d103feffc1d',1,'wininfparser::INFsection']]],
-  ['getvalue_6',['GetValue',['../classwininfparser_1_1_i_n_fsection.html#a93a8a347e4e7d603b995495a6e2af90d',1,'wininfparser::INFsection']]]
+  ['getexactkeyindex_0',['GetExactKeyIndex',['../classwininfparser_1_1_i_n_fsection.html#a68eed7efb016453036a6e68f5be005ba',1,'wininfparser::INFsection']]],
+  ['getfilename_1',['GetFileName',['../classwininfparser_1_1_win_i_n_f.html#ac3209adfcee8d0861bc2b334692a1b0b',1,'wininfparser::WinINF']]],
+  ['getindent_2',['GetIndent',['../classwininfparser_1_1_i_n_fsection.html#a4431d920ce5b7834643d6fdcf2d3dcc7',1,'wininfparser::INFsection']]],
+  ['getkeyindex_3',['GetKeyIndex',['../classwininfparser_1_1_i_n_fsection.html#a60d50fe4fa59cda676ae3cacdbe6570c',1,'wininfparser::INFsection']]],
+  ['getname_4',['GetName',['../classwininfparser_1_1_i_n_fsection.html#a7bbf9ec71e0a6bdf4e9bd6b69e298d84',1,'wininfparser::INFsection']]],
+  ['getsection_5',['GetSection',['../classwininfparser_1_1_win_i_n_f.html#a935a049bb0a9a993833ed5c6ab7d8cde',1,'wininfparser::WinINF']]],
+  ['gettype_6',['GetType',['../classwininfparser_1_1_i_n_fsection.html#a965250b8ca2a217bb3531d103feffc1d',1,'wininfparser::INFsection']]],
+  ['getvalue_7',['GetValue',['../classwininfparser_1_1_i_n_fsection.html#a93a8a347e4e7d603b995495a6e2af90d',1,'wininfparser::INFsection']]]
 ];
